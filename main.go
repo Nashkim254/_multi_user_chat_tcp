c8c9b65caaed9c4f718346f2f7abeb215d53d348
programming6131 <kimutainashon@gmail.com>
2024-08-25 19:55:42 +0300

    chanels

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
@@ -8,9 +9,43 @@ import (
 const port = "6969"
 const StreamerMode = false
 
+type MessageType int
+
+const (
+	ClientConnected MessageType = iota + 1
+	DeleteClient
+	NewMessage
+)
+
+type Message struct {
+	Type MessageType
+	Conn net.Conn
+	Text string
+}
 type Client struct {
 	conn   net.Conn
 	output chan string
+}
+
+func server(messages chan Message) {
+	conns := map[string]net.Conn{}
+	for {
+		msg := <-messages
+		switch msg.Type {
+		case ClientConnected:
+			conns[msg.Conn.RemoteAddr().String()] = msg.Conn
+		case DeleteClient:
+			delete(conns, msg.Conn.RemoteAddr().String())
+			msg.Conn.Close()
+		case NewMessage:
+			for _, conn := range conns {
+				_, err := conn.Write([]byte(msg.Text))
+				if err != nil {
+					fmt.Printf("Could not send data, %s: %s", SafeRemoteAddr(conn), err)
+				}
+			}
+		}
+	}
 }
 
 func SafeRemoteAddr(conn net.Conn) string {
@@ -23,17 +58,25 @@ func SafeRemoteAddr(conn net.Conn) string {
 
 }
 
-func handleConnection(conn net.Conn, outgoing chan string) {
+func client(conn net.Conn, messages chan Message) {
 	defer conn.Close()
 
-	buffer := []byte{}
+	buffer := make([]byte, 512)
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
-			conn.Close()
+			messages <- Message{
+				Type: DeleteClient,
+				Text: string(buffer[0:n]),
+				Conn: conn,
+			}
 			return
 		}
-		outgoing <- string(buffer[0:n])
+		messages <- Message{
+			Type: NewMessage,
+			Text: string(buffer[0:n]),
+			Conn: conn,
+		}
 	}
 }
 func main() {
@@ -42,13 +85,18 @@ func main() {
 		log.Fatalf("Error connecting to: %s on %s", port, err)
 	}
 	log.Printf("Listening to TCP connections on port %s ....\n", port)
+	messages := make(chan Message)
+	go server(messages)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Printf("Could not accept connection %s\n", err)
 		}
 		log.Printf("Accepting connection from %s", SafeRemoteAddr(conn))
-		outgoing := make(chan string)
-		go handleConnection(conn, outgoing)
+		messages <- Message{
+			Type: ClientConnected,
+			Conn: conn,
+		}
+		go client(conn, messages)
 	}
 }
